903846f6ebd8f52c3af896d041480ec6db088cd9
Andrei Nedelcu <andreinedelcu112@gmail.com>
2020-06-08 21:16:47 +0200

    changes in the readme

---
+++ src/components/Todos.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+function Todos() {
+  return (
+    <div >
+      <header className="App-header">
+        
+        
+          <h1>Todos</h1>
+       
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default Todos;
