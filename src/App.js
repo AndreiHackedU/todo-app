3e5e167901fcc8c61810484646b8743583ce159c
Andrei Nedelcu <andreinedelcu112@gmail.com>
2020-06-10 21:39:40 +0200

    calculadora

--- src/App.js
+++ src/App.js
@@ -2,29 +2,81 @@ import React, { useState } from "react";
 import Todos from "./components/Todos";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [currentNumber, setNumber] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{count}</h1>
-        <button
-          type="button"
-          class="btn btn-outline-danger"
-          onClick={() => setCount(count - 1)}
-        >
-          discount
-        </button>
-        <button
-          type="button"
-          class="btn btn-outline-success"
-          onClick={() => setCount(count + 1)}
-        >
-          count
-        </button>
+        <div>
+          <input type="text" id="textinput" value={currentNumber}></input>
+        </div>
+        <div className="numbersRoot">
+          {numbers.map((number) => (
+            <button
+              value={number}
+              key={number}
+              className="btn btn-outline-primary"
+              onClick={(event) => setNumber(event.target.value)}
+            >
+              {number}
+            </button>
+          ))}
+        </div>
+        <div>
+          <button type="button" class="btn btn-outline-primary">
+            7
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            8
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            9
+          </button>
+          <button type="button" class="btn btn-info">
+            x
+          </button>
+        </div>
+        <div>
+          <button type="button" class="btn btn-outline-primary">
+            4
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            5
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            6
+          </button>
+          <button type="button" class="btn btn-info">
+            -
+          </button>
+        </div>
+        <div>
+          <button type="button" class="btn btn-outline-primary">
+            1
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            2
+          </button>
+          <button type="button" class="btn btn-outline-primary">
+            3
+          </button>
+          <button type="button" class="btn btn-info">
+            +
+          </button>
+        </div>
+        <div>
+          <button type="button" class="btn btn-outline-primary">
+            0
+          </button>
+          <button type="button" class="btn btn-outline-success">
+            .
+          </button>
+          <button type="button" class="btn btn-outline-success" id="equal">
+            =
+          </button>
+        </div>
       </header>
     </div>
   );
-}
-
-export default App;
+};
