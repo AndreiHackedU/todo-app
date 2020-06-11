a85657dd9127b9f957c22a5571d1da2d73e75628
Andrei Nedelcu <andreinedelcu112@gmail.com>
2020-06-11 16:04:16 +0200

    calculadora

--- src/App.js
+++ src/App.js
@@ -2,16 +2,34 @@ import React, { useState } from "react";
 
 import "./App.css";
 
+const sum = (a, b) => a + b;
+const difference = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const calculate = (a, b, operator) => {
+  if (operator === "+") {
+    return sum(a, b);
+  } else if (operator === "-") {
+    return difference(a, b);
+  } else {
+    return multiply(a, b);
+  }
+};
+
 export default () => {
   const numbers = [7, 8, 9, "+", 4, 5, 6, "-", 1, 2, 3, "x"];
-  const [currentNumber, setNumber] = useState(0);
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [secondNumber, setSecondNumber] = useState(null);
+  const [result, setResult] = useState(null);
 
   return (
     <div className="App">
       <header className="App-header">
         <div id="calculator">
           <div>
-            <input type="text" id="textinput" value={currentNumber}></input>
+            <h3>{`${firstNumber ? firstNumber : ""} ${
+              operator ? operator : ""
+            } ${secondNumber ? secondNumber : ""} = ${result}`}</h3>
           </div>
           <div className="numbersRoot">
             {numbers.map((number) => (
@@ -19,7 +37,15 @@ export default () => {
                 value={number}
                 key={number}
                 className="btn btn-info"
-                onClick={(event) => setNumber(event.target.value)}
+                onClick={(event) => {
+                  if (!firstNumber) {
+                    setFirstNumber(parseFloat(event.target.value));
+                  } else if (!operator) {
+                    setOperator(event.target.value);
+                  } else {
+                    setSecondNumber(parseFloat(event.target.value));
+                  }
+                }}
               >
                 {number}
               </button>
@@ -34,7 +60,14 @@ export default () => {
               <button type="button" className="btn btn-info">
                 .
               </button>
-              <button type="button" className="btn btn-info" id="equal">
+              <button
+                type="button"
+                className="btn btn-info"
+                id="equal"
+                onClick={() =>
+                  setResult(calculate(firstNumber, secondNumber, operator))
+                }
+              >
                 =
               </button>
             </div>
